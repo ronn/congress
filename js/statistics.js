ce98d6ff79ed4d366216cf236acc411e32667220
ronn <ronnie-filth@hotmail.com>
2019-03-27 18:45:44 +0100

    T3 S3

--- js/statistics.js
+++ js/statistics.js
@@ -1,9 +1,40 @@
-const getNumberMembersByParty = party => getMembersArray("senate")
+const getSenateMembers = () => getMembersArray("senate")
+
+getMembersByParty = party => getSenateMembers()
     .filter(member => member.party === party)
-    .length
+
+const getNumberMembersByParty = party => getMembersByParty(party).length
+
+const getAverageVotes = party => getMembersByParty(party)
+    .map(senator => senator.votes_with_party_pct)
+    .reduce(addVotes) / getSenateMembers().length
+
+const addVotes = (accumulator, votes) => accumulator + votes
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const statistics = {
-    numberOfDemocrats: getNumberMembersByParty("R"),
-    numberOfRepublicans: getNumberMembersByParty("D"),
-    numberOfIndependents: getNumberMembersByParty("I")
+    democrats: {
+        numberOfReps: getNumberMembersByParty("D"),
+        averageVotesWithParty: getAverageVotes("D")
+    },
+    republicans: {
+        numberOfReps: getNumberMembersByParty("R"),
+        averageVotesWithParty: getAverageVotes("R")
+    },
+    independants: {
+        numberOfReps: getNumberMembersByParty("I"),
+        averageVotesWithParty: getAverageVotes("I")
+    },
 }
